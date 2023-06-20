003642f93fcde45ea96cb31065919f6693498d33
legendary <1207588603@qq.com>
2023-06-20 11:22:57 +0800

    完成首页的布局

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="app-container">
-    <h1 class="text-center">天子呼来不上船，自称臣是酒中仙。</h1>
+    <div class="sidebar-container">sidebar-container</div>
+
+    <div class="main-container">
+      <div class="fixed-header">
+        <div class="navbar">navbar</div>
+      </div>
+
+      <div class="app-main">app-main</div>
+    </div>
   </div>
 </template>
 
@@ -11,6 +19,50 @@
   position: relative;
   width: 100%;
   height: 100%;
+}
+
+.sidebar-container {
+  position: fixed;
+  left: 0;
+  top: 0;
+  z-index: 1000;
+  width: $base-sidebar-width;
+  height: 100%;
+  box-shadow: 2px 0 6px rgba(0, 21, 41, 0.35);
+}
+
+.main-container {
+  position: relative;
+  height: 100%;
+  margin-left: $base-sidebar-width;
+  transition: margin-left 0.28s;
+}
+
+.fixed-header {
+  position: fixed;
+  right: 0;
+  top: 0;
+  z-index: 9;
+  width: calc(100% - #{$base-sidebar-width});
+  transition: width 0.28s;
+}
+
+.navbar {
+  position: relative;
+  width: 100%;
+  height: $navbar-height;
+  background-color: #fff;
+  box-shadow: 0 1px 4px rgba(0, 21, 41, 0.08);
   overflow: hidden;
+}
+
+.app-main {
+  position: relative;
+  width: 100%;
+  min-height: calc(100vh - #{$navbar-height});
+  overflow: hidden;
+}
+.fixed-header + .app-main {
+  padding-top: $navbar-height;
 }
 </style>
