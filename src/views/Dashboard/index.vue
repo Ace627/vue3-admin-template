ecd370bf26f3ba8dc0d7651f141dbf31960a519e
legendary <1207588603@qq.com>
2023-06-25 14:31:15 +0800

    早报图片添加 loading 效果

--- src/views/Dashboard/index.vue
+++ src/views/Dashboard/index.vue
@@ -1,10 +1,23 @@
 <template>
-  <div class="app-content">
-    <img src="https://api.03c3.cn/zb" alt="每天60s读懂世界" draggable="false" />
+  <div class="app-content" :class="{ loading: loading }">
+    <img v-if="loading" src="http://www.86y.org/images/loading.gif" alt="loading" />
+    <img v-else src="https://api.03c3.cn/zb" alt="每天60s读懂世界" draggable="false" class="is-loading" />
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const loading = ref(true)
+
+function loadImage() {
+  const img = new Image()
+  img.src = 'https://api.03c3.cn/zb'
+  img.onload = () => {
+    loading.value = false
+  }
+}
+
+loadImage()
+</script>
 
 <style lang="scss" scoped>
 .app-content {
@@ -12,5 +25,11 @@
   justify-content: center;
   align-items: center;
   padding: 6px;
+  &.loading img {
+    position: fixed;
+    width: 64px;
+    height: 64px;
+    margin-top: 128px;
+  }
 }
 </style>
