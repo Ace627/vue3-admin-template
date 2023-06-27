2f96de735f0bc03ff5e4c143cf2c427b5d7fed58
legendary <1207588603@qq.com>
2023-06-27 11:19:08 +0800

    新增追剧地址：思古影视

--- src/router/modules/external.ts
+++ src/router/modules/external.ts
@@ -4,23 +4,25 @@ import type { RouteRecordRaw } from 'vue-router'
 const Layout = () => import('@/layout/index.vue')
 const IFrame = () => import('@/views/iFrame/index.vue')
 
-// 创建外链的方法
-function createLink(path: string, title: string, elIcon = 'Promotion'): RouteRecordRaw {
-  return { path, name: title, component: () => {}, meta: { title, elIcon } }
-}
-
 export default [
   {
     path: '/external',
     name: 'IFrame',
     component: Layout,
     meta: { title: '外链', svgIcon: 'External', alwaysShow: true, order: 3 },
+    redirect: '/external/App影院',
     children: [
       {
         path: 'App影院',
         name: 'App影院',
         component: IFrame,
         meta: { title: 'App影院', frameURL: 'https://www.appmovie.cc', svgIcon: 'Movie' },
+      },
+      {
+        path: '思古影视',
+        name: '思古影视',
+        component: IFrame,
+        meta: { title: '思古影视', frameURL: 'https://sigu.cc', svgIcon: 'Movie' },
       },
     ],
   },
